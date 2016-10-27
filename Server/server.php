c9311148d3ca3d6837192179a70d1f00bd3059a9
memory <service@im050.com>
2016-10-27 10:47:00 +0800

    fix

--- Server/server.php
+++ Server/server.php
@@ -1,12 +1,6 @@
 <?php
 include 'Includes/init.php';
 
-use Utils\Config;
-
-echo Config::get('redis.default.host');
-echo Config::get('redis.default.host');
-echo Config::get('redis.default.host');
-
 // 启动服务
 $main_server = new \Server\MainServer('0.0.0.0', 8888);
 
